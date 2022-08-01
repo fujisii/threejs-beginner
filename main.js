4068f0c5e7f0d98b93ef026585a8d445ce89136e
fujisii <51264835+fujisii@users.noreply.github.com>
2022-08-01 23:24:39 +0900

    Complate Section 4-13 - カメラを作成してみよう

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import * as THREE from "three";
 
-let scene;
+let scene, camera;
 
+// シーンを追加
 scene = new THREE.Scene();
+
+// カメラを追加
+camera = new THREE.PerspectiveCamera(
+    50, // 50度の視野角
+    window.innerWidth / window.innerHeight, // アスペクト比（画面の横幅と縦幅の比率のこと）
+    0.1, // カメラの開始距離
+    1000 // カメラの終了距離
+);
